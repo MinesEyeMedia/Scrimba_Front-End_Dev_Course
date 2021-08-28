19ccd3dc6a27d75e6d5247febc17db577ebcacbf
Jacob <jacobmines@protonmail.com>
2021-08-28 17:45:02 -0400

    Commit #3 - Code Refactoring of 'myLeads()' function.
    Code refactoring. Changed 'renderLeads()' function to 'renderData()' and added a single argument to allow for a more dynamic function. Moved code position of renderData() function.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Notes and updates for the front-end developer course by Scrimba.
 - Commit #1 - Initial Commit.
 
 - Commit #2 - Initial creation and commit of HTML, CSS & JS files that contain code up to the current moment.
+
+- Commit #3 - Code refactoring. Changed 'renderLeads()' function to 'renderData()' and added a single argument to allow for a more dynamic function. Moved code position of renderData() function.
--- _Module-3_MakingWebsitesInteractive/_Code-Testing/script-mod3.js
+++ _Module-3_MakingWebsitesInteractive/_Code-Testing/script-mod3.js
@@ -12,49 +12,51 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem(myLeads))
 // if there are any leads (parsed - as JSON OBJ) in const then assign them to the array 'myLeads' and call 'renderLeads()' to display them.
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    renderLeads(myLeads)
 }
 
-// listens for double-click of the deleteBtn html element.
-// clears localStorage, clears myLeads array, and calls 'renderLeads()'.
-// we call 'renderLeads()' to clear the <li> element(s) rather than clear it manually with innerHTML for a few reasons.
-// 1) the renderLeads() function is already written. 2) it's less computationally expensive.
-// 3) the renderLeads() function wil already be cleared as the 'myLeads' array is already cleared in the below function therefore 'renderLeads()'
-// will have nothing to actually 'render' when it runs 'ulEl.innerHTML = listItems'.
-deleteBtn.addEventListener("dblclick", function () {
-    localStorage.clear()
-    myLeads = []
-    renderLeads()
-})
-
-// on click on button (btnEl) it will .push the .value in the input field (inputEl) to 'myLeads'.
-// will set .value of input field to "" (blank), or reset it.
-// will call renderLeads() function
-saveBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-
-    console.log(localStorage.getItem("myLeads"))
-})
-
-// this function will loop through the input of the 'myLeads' array and then reassign the value into the 'listItems' variable.
-// as seen, it will wrap the values in <li> tags, and then finally change the 'innerHTML' of the 'ul-el' html element with the value
+// this function will loop through the input of the data array as passed as a parameter ('leads') and then reassign the value into the 'listItems' variable.
+// as seen below, it will wrap the values in <li> tags, and then finally change the 'innerHTML' of the 'ul-el' html element with the value -
 // sorted in the variable 'listItems'. We do this this way for better DOM performance. This way the DOM is NOT manipulated x times during the loop.
-function renderLeads() {
+function renderData(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
                     <li>
-                        <a href="${myLeads}" target="_blank">
-                            ${myLeads[i]}
+                        <a href="${leads}" target="_blank">
+                            ${leads[i]}
                         </a>
                     </li>
                     `
     }
     ulEl.innerHTML = listItems
 }
+
+// listens for double-click of the deleteBtn html element.
+// clears localStorage, clears myLeads array, and calls 'renderData()'.
+// we call 'renderData()' to clear the <li> element(s) rather than clear it manually with innerHTML for a few reasons.
+// 1) the renderData() function is already written. 2) it's less computationally expensive.
+// 3) the renderData() function wil already be cleared as the 'myLeads' array is already cleared in the below function therefore 'renderData()'
+// will have nothing to actually 'render' when it runs 'ulEl.innerHTML = listItems'.
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    renderData(myLeads)
+})
+
+// on click on button (btnEl) it will .push the .value in the input field (inputEl) to 'myLeads'.
+// will set .value of input field to "" (blank), or reset it.
+// will call the renderData() function.
+saveBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderData(myLeads)
+
+    console.log(localStorage.getItem("myLeads"))
+})
+
+
 
 
 // localStorage Manipulation.
